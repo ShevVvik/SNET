609178c6a181af58cc31cc09c074ddfd3a511497
ShevVvik <50230838+ShevVvik@users.noreply.github.com>
2019-05-08 16:22:41 +0300

    Revert "Update WebConfig.java"
    This reverts commit bf65dfd9fa17597f63c3768bf0ec4b9628d785de.

--- src/main/java/SNET/springConfig/WebConfig.java
+++ src/main/java/SNET/springConfig/WebConfig.java
@@ -9,8 +9,6 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-
-//Авыфывфывфывфыв
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 /*
