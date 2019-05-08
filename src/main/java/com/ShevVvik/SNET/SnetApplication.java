8ad81ba7f7832ffca8b2730272a8d58d9f410f04
ShevVvik <50230838+ShevVvik@users.noreply.github.com>
2019-05-08 16:22:22 +0300

    Revert "Update SnetApplication.java"
    This reverts commit 74dd820520a1bf349e51bad70cbfa8763b70620e.

--- src/main/java/com/ShevVvik/SNET/SnetApplication.java
+++ src/main/java/com/ShevVvik/SNET/SnetApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Main
 //Main12
-//фывфывфывфыв
 @SpringBootApplication
 public class SnetApplication {
 
