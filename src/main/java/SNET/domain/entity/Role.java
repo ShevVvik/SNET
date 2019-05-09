3c2cb6319ac66d83bf1af9c455d4957e78db5459
ShevVvik <50230838+ShevVvik@users.noreply.github.com>
2019-05-09 19:14:35 +0300

    Fail
    Я сгорел, сделаю заново

--- src/main/java/SNET/domain/entity/Role.java
+++
@@ -1,9 +0,0 @@
-package SNET.domain.entity;
-
-public final class Role {
-
-	public static final String ROLE_ADMIN 		= "ROLE_ADMIN";
-	public static final String ROLE_MANAGER 	= "ROLE_MANAGER";
-	public static final String ROLE_USER 		= "ROLE_USER";
-
-}
